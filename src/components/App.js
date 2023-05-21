b9bc2f0ea2b337b0dd948277f5a86a8b1dee82bf
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-05-22 04:07:25 +0500

    change font location to src

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './App.css';
 import { newFont } from '../styles/FontStyle.js'
 
    const App = () => {
   return (
     <div>
-        <h1 className={newFont}>
+        <h1 className= 'metro-font'>
             Sumair Qaisar
         </h1>
     </div>
