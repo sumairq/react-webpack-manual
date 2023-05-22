d179080a9439635325dc0b8bfb01af449c40262d
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-05-22 13:39:03 +0500

    new fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import './App.css';
-import { newFont } from '../styles/FontStyle.js'
+// import './App.css';
+ import { newFont } from '../styles/FontStyle.js'
+ import { style, fontFace } from 'typestyle';
+
+ console.log('font-face',fontFace({
+    fontFamily: 'SF Pro',
+    src: "url('/fonts/sf-pro-text-regular.woff') format('woff')",
+  }))
 
    const App = () => {
+
+    console.log(fontFace)
   return (
-    <div>
+    <div className={newFont}>
+      
         <h1 className= 'metro-font'>
             Sumair Qaisar
         </h1>
+
+        
     </div>
   )
 }
--- src/styles/FontStyle.js
+++ src/styles/FontStyle.js
@@ -1,17 +1,20 @@
-// import { style, fontFace } from 'typestyle';
+import { style, fontFace } from 'typestyle';
+import myFontUrl from '../../public/fonts/Metropolis-Bold.woff2'
+
+// const metroFontUrl = '../../public/fonts/Metropolis-Bold.woff2'
 
 
-// fontFace({
-//   fontFamily: 'Metropolis',
-//   src: 'url("../fonts/Metropolis-Bold.woff2") format("woff2")',
-// });
+fontFace({
+  fontFamily: 'Metropolis',
+  src: `url(${myFontUrl}) format('woff2')`
+});
 
-// fontFace({
-//   fontFamily: 'SF Pro',
-//   src: 'url("../fonts/sf-pro-text-regular.woff") format("woff")',
-// });
+fontFace({
+  fontFamily: 'SF Pro',
+  src: "url(public/fonts/sf-pro-text-regular.woff) format('woff')",
+});
 
 
-// export const newFont = style({
-//     fontFamily: 'Metropolis'
-// })
+export const newFont = style({  
+    fontFamily: 'Metropolis'
+})
